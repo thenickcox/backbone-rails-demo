a78cf1c80b4ecbaf61ac13a0b020e67c6fd3b0cd
Nick Cox <nick@nickcox.me>
2014-11-25 08:02:52 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+u = User.create!(email: 'admin@test.com', password: 'passw0rd', name: 'System Admin')
+albums = [
+  {
+    artist: 'John Coltrane',
+    title: 'A Love Supreme',
+    year: 1965,
+    song_count: 3,
+    user_id: u.id
+  },
+  {
+    artist: 'Miles Davis',
+    title: 'Kind of Blue',
+    year: 1959,
+    song_count: 5,
+    user_id: u.id
+  },
+  {
+    artist: 'Cannonball Adderley',
+    title: 'Autumn Leaves',
+    year: 1975,
+    song_count: 5,
+    user_id: u.id
+  },
+  {
+    artist: 'Sonny Rollins',
+    title: 'Saxophone Colossus',
+    year: 1956,
+    song_count: 5,
+    user_id: u.id
+  },
+  {
+    artist: 'Thelonius Monk',
+    title: 'Brilliant Corners',
+    year: 1957,
+    song_count: 5,
+    user_id: u.id,
+  },
+  {
+    artist: 'Miles Davis',
+    title: 'Birth of the Cool',
+    year: 1957,
+    song_count: 5,
+    user_id: u.id,
+  },
+  {
+    artist: 'Horace Silver',
+    title: 'Horace Silver and the Jazz Messengers',
+    year: 1955,
+    song_count: 8,
+    user_id: u.id,
+  },
+  {
+    artist: 'John Coltrane',
+    title: 'Blue Train',
+    year: 1957,
+    song_count: 5,
+    user_id: u.id,
+  },
+  {
+    artist: 'Freddie Hubbard',
+    title: 'Ready for Freddie',
+    year: 1961,
+    song_count: 7,
+    user_id: u.id,
+  },
+]
+
+albums.each { |album_attrs| Album.create!(album_attrs) }
+
